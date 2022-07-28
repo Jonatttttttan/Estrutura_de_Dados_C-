8a3fd8119da1ddb47c5b2d47689324484adac1dd
Jonathan <jonathan.iuliany2@gmail.com>
2022-07-28 20:26:34 -0300

    Finalizaçao de pilha dinamica

--- pilha_dinamica/pilhaDinamica.cpp
+++ pilha_dinamica/pilhaDinamica.cpp
@@ -9,13 +9,19 @@ using namespace std;
     }
 
     PilhaDinamica::~PilhaDinamica(){
-
-    }
-   bool PilhaDinamica:: estaCheio(){
-        return NoTopo==nullptr;
+      No* NoTemp;
+      while (NoTopo!=nullptr){
+        NoTemp=NoTopo;
+        NoTopo=NoTopo->proximo;
+        delete NoTemp;
+      }
 
     }
    bool PilhaDinamica:: Vazio(){
+        return NoTopo==nullptr;
+
+    }
+   bool PilhaDinamica:: estaCheio(){
         No* NoNovo;
         try{
             NoNovo = new No;
@@ -30,9 +36,43 @@ using namespace std;
 
     
 
-    void PilhaDinamica:: inserir(TipoItem item){}
+    void PilhaDinamica:: inserir(TipoItem item){
 
-    TipoItem PilhaDinamica:: remover(){}
+        if(estaCheio()){
+            cout << "Pilha cheia, impossivel inserir elemento";
+        }else{
+            No* NoNovo = new No;
+            NoNovo->valor = item;
+            NoNovo->proximo = NoTopo;
+            NoTopo=NoNovo;
+        }
+    }
 
-    void PilhaDinamica:: imprimir(){}
+    TipoItem PilhaDinamica:: remover(){
+        if(Vazio()){
+            cout << "A pilha esta vazia";
+            return 0;
+        }else{
+            No* NoTemp;
+            NoTemp = NoTopo;
+            TipoItem item=NoTopo->valor;
+            NoTopo = NoTopo->proximo;
+            delete NoTemp;
+
+            return item;
+
+        }
+    }
+
+    void PilhaDinamica:: imprimir(){
+        cout << "im";
+        No* NoTemp;
+        NoTemp=NoTopo;
+        while(NoTemp!=nullptr){
+            cout << NoTemp->valor;
+            NoTemp=NoTemp->proximo;
+            cout << "listando";
+        }
+       delete NoTemp;
+    }
     
