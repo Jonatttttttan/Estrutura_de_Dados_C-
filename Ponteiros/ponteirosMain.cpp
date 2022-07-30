aeb3546fca80f7b9081332529534289e892febdf
Jonathan <jonathan.iuliany2@gmail.com>
2022-07-30 00:37:30 -0300

    Adicionando codigo de ponteiros

---
+++ Ponteiros/ponteirosMain.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+int x;
+x=50;
+int* pt;
+pt=&x;
+cout << "O endereco de x e: ";
+ cout << pt;
+ cout << "o valor de x é: ";
+  cout << *pt;
+
+
+
+
+
+
+return 0;
+}
